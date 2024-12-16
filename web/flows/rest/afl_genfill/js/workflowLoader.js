239b775717b84e23696db07cd01fca3563e0efed
Nomi <7894324+nomiscientist@users.noreply.github.com>
2024-12-16 06:34:34 +0000

    flows del

--- web/flows/rest/afl_genfill/js/workflowLoader.js
+++
@@ -1,7 +0,0 @@
-export async function loadWorkflow(wfpath) {
-    const response = await fetch(wfpath);
-    if (!response.ok) {
-        throw new Error('Failed to load workflow: ' + response.statusText);
-    }
-    return await response.json();
-}
